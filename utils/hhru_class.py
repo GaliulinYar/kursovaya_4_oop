8522a554a6ccb63558e352cf7496bbf7d875ba48
Yaroslav Galiulin <yaroslav66@list.ru>
2023-06-14 21:24:36 +0500

    Дописал класс получения вакансий с хх ру, добавил метод записи данных в файл джейсон

--- utils/hhru_class.py
+++ utils/hhru_class.py
@@ -1,74 +1,81 @@
 import json
 from abc import ABC
-from pprint import pprint
-
 import requests
-
 from utils.get_hh_info import AbstractJobPlatform
 
-class JobListing:
-    def __init__(self, title, link, salary, description):
-        self.title = title
-        self.link = link
-        self.salary = salary
-        self.description = description
 
-    def __str__(self):
-        return f"Title: {self.title}Link: {self.link}Salary: {self.salary}Description: {self.description}"
-
-
-class HHJobPlatform= str(JobListing(title, link, salary, description))(AbstractJobPlatform, ABC):
+class HHJobPlatform(AbstractJobPlatform, ABC):
 
     def __init__(self, keyword):
         self.keyword = keyword
+        self.salary_min = None
+
+    def __gt__(self, other):
+        return int(self.salary_min) > int(other.salary_min)
+
+    def __ge__(self, other):
+        return int(self.salary_min) >= int(other.salary_min)
+
+    def __lt__(self, other):
+        return int(self.salary_min) < int(other.salary_min)
+
+    def __le__(self, other):
+        return int(self.salary_min) <= int(other.salary_min)
+
+    def __eq__(self, other):
+        return int(self.salary_min) == int(other.salary_min)
 
     def connect(self, params=None):
         # Реализация подключения к API hh.ru
-        #base_url = f"https://hh.ru/search/vacancy?text={self.keyword}"
-        # self.headers = {"https://hh.ru/search/vacancy": self.keyword}
+
         url = 'https://api.hh.ru/vacancies'
-        params = {'text': self.keyword, 'area': 1, "per_page": 10}
+        params = {'text': self.keyword,  # Ключевое слово для поиска ваканчий
+                  'area': 1,  # Индекс города для поиска 1(Москва)
+                  "per_page": 10  # Кол-во вакансий на странице
+                  }
         headers = {
             "User-Agent": "50355527",  # Replace with your User-Agent header
         }
 
         response = requests.get(url, params=params, headers=headers)
 
-
-        #response = requests.get(base_url, params=params)
-
         return response
 
-    def get_jobs(self):
-        # Получение вакансий с hh.ru
-        # url = 'https://api.hh.ru/vacancies'
-        # params = {'text': query, 'area': 1}
-        # response = requests.get(url, params=params)
+    def get_jobs(self, **kwargs):
+        # Метод создания словаря вакансий
         if self.connect().status_code == 200:
             data = self.connect().json()
-            jobs = []
+            jobs = {}
             for item in data['items']:
                 title = item['name']
-                link = item['url']
-                salary = item['salary']['from'] if item['salary'] and 'from' in item['salary'] else None
+                link = item['alternate_url']
+
+                if item['salary']:
+                    salary_min = item['salary']['from']
+                    salary_max = item['salary']['to']
+
+                else:
+                    salary_min = None
+                    salary_max = None
+
                 description = item['snippet']['requirement'] if item['snippet'] and 'requirement' in item[
                     'snippet'] else None
-                job = str(JobListing(title, link, salary, description))
-                jobs.append(job)
-            return jobs
 
+                jobs = {
+                    'title': title,
+                    'link': link,
+                    'salary_min': salary_min,
+                    'salary_max': salary_max,
+                    'description': description
+                }
+                print(jobs)
+                self.write_file_vacancy(jobs)
+            return jobs
 
         else:
             print(f"Request failed with status code: {self.connect().status_code}")
 
-
-huemoe = HHJobPlatform('python')
-pprint(huemoe.get_jobs())
-#print(huemoe.get_jobs())
-
-#hfhf = JobListing('title', 'link', 'salary', 'description')
-#print(str(hfhf))
-
-
-
+    def write_file_vacancy(self, jobs):
+        with open('vacancy_list.json', 'a', encoding='utf-8') as json_file:
+            json.dump(jobs, json_file, ensure_ascii=False)
 
