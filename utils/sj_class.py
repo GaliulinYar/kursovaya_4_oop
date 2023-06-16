e3230b1c9f714d6db5c76dfe2866fd5d1f3eec69
Yaroslav Galiulin <yaroslav66@list.ru>
2023-06-16 20:35:38 +0500

    Создание класса для сбора вакансий с сайта суперджоп

--- utils/sj_class.py
+++ utils/sj_class.py
@@ -1,8 +1,81 @@
-class SuperJobPlatform(AbstractJobPlatform):
+import json
+import os
+from abc import ABC
+from pprint import pprint
+
+import requests
+from utils.get_hh_info import AbstractJobPlatform
+
+
+class SuperJobPlatform(AbstractJobPlatform, ABC):
+
+    def __init__(self, keyword):
+        self.keyword = keyword
+        self.salary_min = None
+
+    def __gt__(self, other):
+        return int(self.salary_min) > int(other.salary_min)
+
+    def __ge__(self, other):
+        return int(self.salary_min) >= int(other.salary_min)
+
+    def __lt__(self, other):
+        return int(self.salary_min) < int(other.salary_min)
+
+    def __le__(self, other):
+        return int(self.salary_min) <= int(other.salary_min)
+
+    def __eq__(self, other):
+        return int(self.salary_min) == int(other.salary_min)
+
     def connect(self):
         # Реализация подключения к API superjob.ru
-        pass
+        headers = {
+            'Host': 'api.superjob.ru',
+            'X-Api-App-Id': os.getenv('API_KEY'),
+            'Authorization': 'Bearer r.000000010000001.example.access_token',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        params = {"count": 3, "page": None,
+                  "keyword": self.keyword, "archive": False, }
+        data = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=headers, params=params).json()
+        return data
 
-    def get_jobs(self, query):
+    def get_jobs(self, **kwargs):
         # Получение вакансий с superjob.ru
-        pass
+        # if self.connect().status_code != 400:
+        data = self.connect()
+        list_job = []
+        for item in data['objects']:
+            title = item['profession']
+            link = item['link']
+
+            if item['payment_from']:
+                salary_min = item['payment_from']
+                salary_max = item['payment_to']
+
+            else:
+                salary_min = None
+                salary_max = None
+
+            description = item['candidat']
+
+            jobs = {
+                'title': title,
+                'link': link,
+                'salary_min': salary_min,
+                'salary_max': salary_max,
+                'description': description
+            }
+            list_job.append(jobs)
+        self.write_file_vacancy(list_job)
+        return list_job
+
+    def write_file_vacancy(self, jobs):
+        with open('vacancy_list.json', 'w', encoding='utf-8') as json_file:
+            json.dump(jobs, json_file, sort_keys=False, indent=4, ensure_ascii=False)
+
+
+ads = SuperJobPlatform('python')
+pprint(ads.get_jobs())
+
