58ebeaccfdcdb90da8287f262e76c0530ad8f841
Yaroslav Galiulin <yaroslav66@list.ru>
2023-06-18 18:42:01 +0500

    Создание класса и списка классов вакансий для дальнейшей работы

--- utils/vacancy_class.py
+++ utils/vacancy_class.py
@@ -1,16 +1,52 @@
 
 class Vacancy:
     """Класс создающий экземпляры класса вакансий"""
-    def __init__(self, title, link, salary_min, salary_max, description):
+    all_class_vacancy = []
+
+    def __init__(self, id_vacancy, title, link, salary_min, salary_max, description):
+        self.id = id_vacancy
         self.title = title
         self.link = link
         self.description = description
         self.salary_min = salary_min
         self.salary_max = salary_max
 
+        Vacancy.all_class_vacancy.append(self)
+
+    def __gt__(self, other):
+        return int(self.salary_min) > int(other.salary_min)
+
+    def __ge__(self, other):
+        return int(self.salary_min) >= int(other.salary_min)
+
+    def __lt__(self, other):
+        return int(self.salary_min) < int(other.salary_min)
+
+    def __le__(self, other):
+        return int(self.salary_min) <= int(other.salary_min)
+
+    def __eq__(self, other):
+        return int(self.salary_min) == int(other.salary_min)
 
     def __str__(self):
-        return f"Заголовок вакансии: {self.title} " \
-               f"Ссылка на вакансию: {self.link} " \
-               f"Зарплата от {self.salary_min} до {self.salary_max} " \
-               f"Описание вакансии: {self.description}"
+        return f"id: {self.id}\n" \
+               f"Заголовок вакансии: {self.title}\n" \
+               f"Ссылка на вакансию: {self.link}\n" \
+               f"Зарплата от {self.salary_min} до {self.salary_max}\n" \
+               f"Описание вакансии: {self.description}\n"
+
+    @classmethod
+    def class_vacancy_ex(cls, reader):
+
+        for row in reader:
+            cls(row['id'], row['title'], row['link'], row['salary_min'], row['salary_max'], row['description'])
+
+    #def get_class(self, list_job):
+    #    self.class_vacancy_ex(list_job)
+
+
+#reyrrukfh = Vacancy(title='Разработчик', link='ссылка тут', salary_min='3', salary_max='5', description='уассвсыуцувсусу', id_vacancy='343545')
+#print(str(reyrrukfh))
+
+
+
